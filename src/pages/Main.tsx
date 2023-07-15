3e025b239271ed012c065e32c46ff2aa41b1a3b7
a-small-coder <hulktv222@mail.ru>
2023-07-15 20:37:07 +0300

    :sparkles: added counter

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,11 @@
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import { StoreContext } from '../store/store.context'
 
 function MainPage() {
+
+  const [count, setCount] = useState(0)
 
   const {userStore} = useContext(StoreContext)
     const {logout} = userStore
@@ -26,6 +28,13 @@ function MainPage() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+
+        <button
+          onClick={() => setCount(count + 1)}
+        >
+          count is {count}
+        </button>
+
         <p>
          main
         </p>
