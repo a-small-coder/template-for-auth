b45cb0764717188b3771b4dab6ed4a51838d4fe9
a-small-coder <hulktv222@mail.ru>
2023-07-15 19:47:05 +0300

    🏗️ build routing architecture

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,35 @@
 import {Routes, Route} from 'react-router-dom'
 import sitePath from './routesConsts'
+import ErrorPage from '../pages/Error'
+import MainPage from '../pages/Main'
+import LoginPage from '../pages/Login'
+import RegisterPage from '../pages/Register'
+import ProtectedRoute from './ProtectedRoute'
+import WithoutUserRoute from './WithoutUserRoute'
 
-const Routes = () => {
+const Router = () => {
 
     return (
         <Routes>
-            <Route path={sitePath.main}/>
-            <Route path={sitePath.login}/>
-            <Route path={sitePath.register}/>
-            <Route path={sitePath.error}/>
-            <Route path='*'/>
+            <Route path={sitePath.main} element={
+                <ProtectedRoute>
+                    <MainPage/>
+                </ProtectedRoute>
+            }/>
+            <Route path={sitePath.login} element={
+                <WithoutUserRoute>
+                    <LoginPage/>
+                </WithoutUserRoute>
+            }/>
+            <Route path={sitePath.register} element={
+                <WithoutUserRoute>
+                    <RegisterPage/>
+                </WithoutUserRoute>
+
+            }/>
+            <Route path={sitePath.error} element={<ErrorPage/>}/>
+            <Route path='*' element={<ErrorPage/>}/>
         </Routes>
     )
 }
+export default Router
